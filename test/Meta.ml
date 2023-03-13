f5898a182abed7518c616e7908a4dd7d51773ac1
dehaldat <vlad.ptv@protonmail.com>
2023-03-13 18:12:00 +0100

    Auto-generated tests over code reduction.

--- test/Meta.ml
+++ test/Meta.ml
@@ -1,18 +1,18 @@
-open MiniglocaLib
+open MiniglocaLib.Ast
 open MiniglocaLib.Meta
 
 let deadcode_elimination_test_gloca tag ast =
-  let ddc = deadcode_elimination ast in
-  let check = incr_deadcode_elimination ast in
-  Alcotest.(check (option (Alcotest.testable Ast.pp_s Ast.equal_s)))
-    tag ddc check
+  let nv_reduction = deadcode_elimination ast in
+  let ic_reduction = incr_deadcode_elimination ast in
+  Alcotest.(check (option (Alcotest.testable pp_s equal_s)))
+    tag nv_reduction ic_reduction
 
-let deadcode_elimination_test () =
-  deadcode_elimination_test_gloca "Deadcode elimination test #1"
+let deadcode_elimination_test () = Generator.iterate_on 2 deadcode_elimination_test_gloca 0 10
+  (* deadcode_elimination_test_gloca "Deadcode elimination test #1"
     (Test_1.gloca ());
 
   deadcode_elimination_test_gloca "Deadcode elimination test #2"
     (Test_2.gloca ());
     
   deadcode_elimination_test_gloca "Deadcode elimination test #3"
-    (Test_3.gloca ())
+    (Test_3.gloca ()) *)
