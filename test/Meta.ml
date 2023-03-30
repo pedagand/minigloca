3374aa3b2ff3aa3d3330afb60c0b4e8d7c686769
dehaldat <vlad.ptv@protonmail.com>
2023-03-30 14:13:59 +0200

    Meta tests with no-skip reduction.

--- test/Meta.ml
+++ test/Meta.ml
@@ -4,7 +4,7 @@ open MiniglocaLib.Meta
 let deadcode_elimination_test_gloca tag ast =
   let nv_reduction = deadcode_elimination ast in
   let ic_reduction = incr_deadcode_elimination ast in
-  Alcotest.(check (option (Alcotest.testable pp_s equal_s)))
+  Alcotest.(check ((Alcotest.testable pp_s equal_s)))
     tag nv_reduction ic_reduction
 
 let deadcode_elimination_test () = Generator.iterate_on 4 deadcode_elimination_test_gloca 0 30
